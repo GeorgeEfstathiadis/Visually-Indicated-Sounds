7510dc66e1a3765d41620bf33f49c18afc351843
Georgios Efstathiadis <gioefst2000@gmail.com>
2023-05-11 14:53:24 -0400

    add variation to first model

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision import models
 import torchaudio.transforms as T
+import torchvision.models.video as video_models
 
 from constants import AUDIO_SAMPLE_RATE
 
@@ -77,6 +78,77 @@ class VideoAudioMatchingModel(nn.Module):
         combined = torch.cat((video_output, audio_features), dim=1)
         output = torch.sigmoid(self.fc(combined))
         return output
+
+class VideoAudioMatchingModel2(nn.Module):
+    """
+    Model that combines video and audio sub-models to predict whether a video and audio clip match
+
+    ...
+
+    Description
+    -----------
+    The video sub-model combines:
+    * A ResNet18 model (pre-trained on ImageNet) to extract features from each frame with the last 
+    fully connected layer removed
+    * A GRU to uses the features from each frame to extract a single feature vector for the video
+
+    The audio sub-model combines:
+    * A MelSpectrogram to extract a spectrogram from the audio clip
+    * A ResNet18 model (pre-trained on ImageNet) to extract features from the spectrogram with the last
+    fully connected layer removed
+
+    The video and audio sub-models are combined by concatenating the video feature vector and audio feature
+    and passing it through a fully connected layer to get a single output value.
+    """
+    def __init__(self):
+        super(VideoAudioMatchingModel2, self).__init__()
+        
+        # Video sub-model (ResNet3D)
+        self.video_cnn = video_models.r3d_18(pretrained=True)
+        num_features = self.video_cnn.fc.in_features
+        self.video_cnn.fc = nn.Identity()
+        # freeze all layers of resnet except last layer
+        for param in self.video_cnn.parameters():
+            param.requires_grad = False
+        for param in self.video_cnn.fc.parameters():
+            param.requires_grad = True
+        self.video_gru = nn.GRU(input_size=num_features, hidden_size=512, num_layers=1, batch_first=True)
+
+        # Audio sub-model (Transfer learning from torchaudio)
+        self.audio_preprocess = nn.Sequential(
+            T.MelSpectrogram(sample_rate=AUDIO_SAMPLE_RATE, n_fft=2048, hop_length=512, n_mels=128),
+            T.AmplitudeToDB()
+        )
+        self.audio_cnn = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        self.audio_cnn.fc = nn.Linear(num_features, 512)
+        for param in self.audio_cnn.parameters():
+            param.requires_grad = False
+        for param in self.audio_cnn.fc.parameters():
+            param.requires_grad = True
+        
+        # Combine sub-models
+        self.fc = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 1)
+        )
+
+    def forward(self, video, audio):
+        batch_size, seq_len, c, h, w = video.size()
+        video = torch.transpose(video, 1, 2) # (batch_size, channels, time, height, width)
+        video_features = self.video_cnn(video) 
+        video_output = video_features.view(video_features.size(0), -1)  # flattening
+
+        spectrogram = self.audio_preprocess(audio)
+        spectrogram = spectrogram.unsqueeze(1) # add channel dimension
+        spectrogram_3channel = spectrogram.repeat(1, 3, 1, 1)
+        audio_features = self.audio_cnn(spectrogram_3channel) # 512
+
+        combined = torch.cat((video_output, audio_features), dim=1)
+        output = torch.sigmoid(self.fc(combined))
+        return output
+
 
 
 class VideoAudioMatchingModelConv(nn.Module):
